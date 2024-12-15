de85292e3f8bf62d2fa63ae1ae6e0c08470d847f
ali amin <165170471+aliaminsalah@users.noreply.github.com>
2024-12-15 18:12:03 +0200

    Add files via upload
    add scripts & terraform aws

---
+++ script-terraform.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+echo "Hello, This is my final project for Dhub!"
+
+#Env
+DOCKER_REGISTRY='aliamin10'
+IMAGE_NAME='webweather'
+CONTAINER_NAME='weather-app'
+DOCKER_PORT='5000'
+
+#Stages
+
+echo "===== First Stage: Build Image ====="
+docker build -t ${DOCKER_REGISTRY}/${IMAGE_NAME}:latest .
+if [ $? -eq 0 ]; then
+    echo "Image built successfully."
+else
+    echo "Failed to build image."
+    exit 1
+fi
+
+echo "===== Second Stage: Run Container ====="
+docker rm -f ${CONTAINER_NAME} || true
+docker run -d --name ${CONTAINER_NAME} -p ${DOCKER_PORT}:${DOCKER_PORT} ${DOCKER_REGISTRY}/${IMAGE_NAME}:latest
+if [ $? -eq 0 ]; then
+    echo "Container is running."
+else
+    echo "Failed to run container."
+    exit 1
+fi
+
+echo "===== Third Stage: Test Container ====="
+docker ps | grep ${CONTAINER_NAME} > /dev/null
+if [ $? -eq 0 ]; then
+    echo "Container is running correctly."
+else
+    echo "Container test failed."
+    exit 1
+fi
+
+echo "===== Fourth Stage: Push Image ====="
+docker push ${DOCKER_REGISTRY}/${IMAGE_NAME}:latest
+if [ $? -eq 0 ]; then
+    echo "Image pushed successfully."
+else
+    echo "Failed to push image."
+    exit 1
+fi
+
+echo "===== Cleaning Up Local Image ====="
+docker rmi ${DOCKER_REGISTRY}/${IMAGE_NAME}:latest
+if [ $? -eq 0 ]; then
+    echo "Local image removed successfully."
+else
+    echo "Failed to remove local image."
+    exit 1
+fi
+
+echo "===== Fifth Stage: Play Terraform ====="
+terraform init
+terraform plan
+terraform apply -auto-approve
+if [ $? -eq 0 ]; then
+    echo "Terraform executed successfully."
+else
+    echo "Terraform execution failed."
+   exit 1
+fi
+
+echo "Pipeline completed successfully!"
+
