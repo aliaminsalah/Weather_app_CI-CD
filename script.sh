bda6f177580ee5aa76c472cbc5e93d78e901a1e1
ali amin <165170471+aliaminsalah@users.noreply.github.com>
2024-12-17 20:59:53 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,9 @@
 
 echo "Hello, This is my final project for Dhub!"
 
+GREEN="\033[32m"
+RESET='\033[0m'
+
 #Env
 DOCKER_REGISTRY='aliamin10'
 IMAGE_NAME='webweather'
@@ -11,9 +14,9 @@ DOCKER_PORT='5000'
 #Stages
 
 echo "===== First Stage: Build Image ====="
-docker build -t ${DOCKER_REGISTRY}/${IMAGE_NAME}:latest .
+docker build -t ${IMAGE_NAME}:latest .
 if [ $? -eq 0 ]; then
-    echo "Image built successfully."
+     echo -e "${GREEN}Image built successfully.${RESET}"
 else
     echo "Failed to build image."
     exit 1
@@ -23,7 +26,7 @@ echo "===== Second Stage: Run Container ====="
 docker rm -f ${CONTAINER_NAME} || true
 docker run -d --name ${CONTAINER_NAME} -p ${DOCKER_PORT}:${DOCKER_PORT} ${DOCKER_REGISTRY}/${IMAGE_NAME}:latest
 if [ $? -eq 0 ]; then
-    echo "Container is running."
+    echo -e "${GREEN}Container is running.${RESET}"
 else
     echo "Failed to run container."
     exit 1
@@ -32,7 +35,7 @@ fi
 echo "===== Third Stage: Test Container ====="
 docker ps | grep ${CONTAINER_NAME} > /dev/null
 if [ $? -eq 0 ]; then
-    echo "Container is running correctly."
+    echo -e "${GREEN}Container is running correctly.${RESET}"
 else
     echo "Container test failed."
     exit 1
@@ -41,16 +44,16 @@ fi
 echo "===== Fourth Stage: Push Image ====="
 docker push ${DOCKER_REGISTRY}/${IMAGE_NAME}:latest
 if [ $? -eq 0 ]; then
-    echo "Image pushed successfully."
+   echo -e "${GREEN}Image pushed successfully.${RESET}"
 else
-    echo "Failed to push image."
-    exit 1
+   echo "Failed to push image."
+   exit 1
 fi
 
 echo "===== Cleaning Up Local Image ====="
-docker rmi ${DOCKER_REGISTRY}/${IMAGE_NAME}:latest
+docker rmi ${IMAGE_NAME}:latest
 if [ $? -eq 0 ]; then
-    echo "Local image removed successfully."
+    echo -e "${GREEN}Local image removed successfully.${RESET}"
 else
     echo "Failed to remove local image."
     exit 1
@@ -59,10 +62,10 @@ fi
 echo "===== Fifth Stage: Play Ansible ====="
 ansible-playbook -i inventory playbook.yml
 if [ $? -eq 0 ]; then
-    echo "Ansible playbook executed successfully."
+    echo -e "${GREEN}Ansible executed successfully.${RESET}"
 else
     echo "Ansible playbook execution failed."
    exit 1
 fi
 
-echo "Pipeline completed successfully!"
+echo -e "${GREEN}Pipeline completed successfully!${RESET}"
